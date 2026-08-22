6d703d60130962ed6c863bec77b65c7c5835d92d
nutedabb <topeyemi20.ho@gmail.com>
2026-08-22 07:14:39 -0400

    emotion function created

---
+++ emotion_detection.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    """ this fuction checks the emotion of a given 
+    word and returns a dictionary
+    """
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    Input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(URL, json=Input_json, headers = Headers)
+    cleaned_response = json.loads(response.text)
+    return(cleaned_response)
+
+
+emotion_detector("I am happy")
