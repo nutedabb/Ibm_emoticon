5fea77110b969c8805db805c54c84f2e9af4b421
nutedabb <topeyemi20.ho@gmail.com>
2026-08-22 08:57:20 -0400

    Qustion 3 in prgogress

--- emotion_detection.py
+++ emotion_detection.py
@@ -10,6 +10,14 @@ def emotion_detector(text_to_analyze):
     Input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json=Input_json, headers = Headers)
     cleaned_response = json.loads(response.text)
-    print(cleaned_response)
+    cleaned_response = cleaned_response['emotionPredictions'][0]["emotion"]
+    dominant_emotion = ""
+    for k,v in cleaned_response.items():
+        if max(cleaned_response.values()) == v:
+            dominant_emotion = k
+            
 
+    final_response = {**cleaned_response,"dominant_emotion":dominant_emotion}
+    print(final_response)
+   
 
